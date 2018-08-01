97b167f7b582b3a552126d507bf9d50c8e11c1f1
rohit0147 <rohitchoudhary0147@gmail.com>
2018-08-01 21:18:58 +0530

    q2_2

--- DIP_lab/Assignment1/2/codes/myLinearContrastStretching.m
+++ DIP_lab/Assignment1/2/codes/myLinearContrastStretching.m
@@ -1,46 +1,51 @@
-I=imread('../data/barbara.png');
-% figure, imshow(I);
+function []=myLinearContrastStretching(img,flag)
+    image=imread(img);
+    if flag==0
+        
+        min1=min(min(image));
+        max1=max(max(image));
+        new_image = round((image-min1)*(255/(max1-min1)));
+        myNumOfColors=200;
+        myColorScale = [(0:1/(myNumOfColors-1):1)',(0:1/(myNumOfColors-1):1)',(0:1/(myNumOfColors-1):1)'];
+        colormap(myColorScale);
+        colormap gray;
+        subplot(1,2,1);
+        imshow(image),colorbar;
+        subplot(1,2,2);
+        imshow(new_image),colorbar;
+    else 
+        I_red= image(:,:,1);
+        I_green = image(:,:,2);
+        I_blue = image(:,:,3);
 
-min1=min(min(I));
-max1=max(max(I));
-New_I=round((I-min1)*(255/(max1-min1)));
+        min1_red=min(min(I_red));
+        max1_red=max(max(I_red));
+        New_I_red=round((I_red-min1_red)*(255/(max1_red-min1_red)));
 
-% figure, imshow(New_I);
+        min1_green=min(min(I_green));
+        max1_green=max(max(I_green));
+        New_I_green=round((I_green-min1_green)*(255/(max1_green-min1_green)));
 
-I_colored=imread('../data/canyon.png');
-% figure, imshow(I_colored);
+        min1_blue=min(min(I_blue));
+        max1_blue=max(max(I_blue));
+        New_I_blue=round((I_blue-min1_blue)*(255/(max1_blue-min1_blue)));
 
-I_red= I_colored(:,:,1);
-I_green = I_colored(:,:,2);
-I_blue = I_colored(:,:,3);
-
-min1_red=min(min(I_red));
-max1_red=max(max(I_red));
-New_I_red=round((I_red-min1_red)*(255/(max1_red-min1_red)));
-
-
-min1_green=min(min(I_green));
-max1_green=max(max(I_green));
-New_I_green=round((I_green-min1_green)*(255/(max1_green-min1_green)));
-
-min1_blue=min(min(I_blue));
-max1_blue=max(max(I_blue));
-New_I_blue=round((I_blue-min1_blue)*(255/(max1_blue-min1_blue)));
-
-New_I_colored(:,:,1) = New_I_red;
-New_I_colored(:,:,2) = New_I_green;
-New_I_colored(:,:,3) = New_I_blue;
+        New_I_colored(:,:,1) = New_I_red;
+        New_I_colored(:,:,2) = New_I_green;
+        New_I_colored(:,:,3) = New_I_blue;
+        
+        myNumOfColors=200;
+        myColorScale = [(0:1/(myNumOfColors-1):1)',(0:1/(myNumOfColors-1):1)',(0:1/(myNumOfColors-1):1)'];
+        colormap(myColorScale);
+        colormap jet;
+        subplot(1,2,1);
+        imshow(image),colorbar;
+        subplot(1,2,2);
+        imshow(New_I_colored),colorbar;
+        
+        
+    end
 
 
-figure, imshow(New_I_colored);
-
-myNumOfColors=200;
-myColorScale = [(0:1/(myNumOfColors-1):1)',(0:1/(myNumOfColors-1):1)',(0:1/(myNumOfColors-1):1)'];
-imshow(mat2gray(New_I_colored)),colorbar;
-colormap(myColorScale);
-colormap jet;
-daspect([1,1,1]);
-axis tight;
-colorbar
-% subplot(1,2,2);
-% imshow(mat2gray(newim)),colorbar;
+        
+end
