6d7900130d079bed8b84c97d75332ec9a924d04d
vg-27 <vikrant1garg@gmail.com>
2018-08-18 22:06:30 +0530

    ques 2 code done

--- DIP_lab/160050030_160050031_160050033_assignment2_Filtering/2/code/myMainScript.m
+++ DIP_lab/160050030_160050031_160050033_assignment2_Filtering/2/code/myMainScript.m
@@ -2,5 +2,13 @@
 
 tic;
 %% Your code here
-
+figure(1)
+[a1,mat_fltr] = myBilateralFiltering('../data/grassNoisy.mat',2,15,2);
+figure(2)
+% [a2,~] = myBilateralFiltering('../data/grassNoisy.mat',2,12,2);
+% figure(3)
+% [a3,~] = myBilateralFiltering('../data/grassNoisy.mat',2,13,2);
+figure(4)
+imshow(mat_fltr)
+colorbar
 toc;
