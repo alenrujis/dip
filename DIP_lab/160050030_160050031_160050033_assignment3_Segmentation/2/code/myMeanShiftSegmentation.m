10a94c37806fb50591d377e7c8b4b49cee644384
vg-27 <vikrant1garg@gmail.com>
2018-09-01 12:28:32 +0530

    update in Knn search and appKernal

--- DIP_lab/160050030_160050031_160050033_assignment3_Segmentation/2/code/myMeanShiftSegmentation.m
+++ DIP_lab/160050030_160050031_160050033_assignment3_Segmentation/2/code/myMeanShiftSegmentation.m
@@ -3,8 +3,8 @@
 %   Detailed explanation goes here
 %%%%%%%%Test block%%%%%%%%%%%%%
 filename = '../data/baboonColor.png';
-hr = 3;
-hs = 3;
+hr = 10;
+hs = 10;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % read image
 im= imread(filename);
@@ -16,7 +16,7 @@ im = imgaussfilt(im,1);
 % convolution = imfilter(im,A,'conv');
 
 % resize image
-im=imresize(im,.5);
+im=imresize(im,.25);
 
 
 [row,col,~] = size(im);
@@ -24,26 +24,36 @@ upImg = zeros(row,col,3);
 % selecting n nearest neighbours
 n = 10;
 
+% gradient steps
+tau = 3;
+%%
+wait = waitbar(0,'Wait');
+
 for z = 1:20
 %%
     % 3D matrix to 2D matrix
     X = reshape(im,[],3);
     % substep for finding Knn
     X = cast(X,'double');
-    Mdl = ExhaustiveSearcher(X);
+    Mdl = KDTreeSearcher(X);
     for i = 1:row
         for j = 1:col
-            idx = knnsearch(Mdl,cast(reshape(im(i,j,:),[],3),'double'),'K',n); % finding indexes of k nearest nbrs to (i,j) pixel by color
-            fun = @(x) appKernal(x,i,j,X,hs,hr);
-            res = arrayfun(fun,idx); % each element in res is K(space)*K(color) for each nearest nbr to (i,j)
-            fun2 = @(x,y) X(x)*y;
-            smth = arrayfun(fun2,idx,res);
+            [idx,D] = knnsearch(Mdl,cast(reshape(im(i,j,:),[],3),'double'),'K',n); % finding indexes of k nearest nbrs to (i,j) pixel by color
+            fun = @(x,y) appKernal(x,i,j,y,hs,hr,row);
+            res = arrayfun(fun,idx,D); % each element in res is K(space)*K(color) for each nearest nbr to (i,j)
+            fun2 = @(x,y) X(x,:)*y;
+            smth = arrayfun(fun2,idx,res,'un',0);
     %         meanShift = sum(smth)/sum(res) - im(i,j,:);
-            upImg(i,j,:) = sum(smth)/sum(res);
+            rgb = sum(reshape(cell2mat(smth),3,[]),2)/sum(res);
+            upImg(i,j,1) = tau*(rgb(1) - im(i,j,1)) + im(i,j,1);
+            upImg(i,j,2) = tau*(rgb(2) - im(i,j,2)) + im(i,j,2);
+            upImg(i,j,3) = tau*(rgb(3) - im(i,j,3)) + im(i,j,3);
         end
     end
     im = upImg;
+    waitbar(z/20,wait,'chalne do');
 end
+close(wait)
 %%
 % end
 
