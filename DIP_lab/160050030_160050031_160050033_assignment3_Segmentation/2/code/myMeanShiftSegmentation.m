618c9fa7b5530e8ee72a05832f5a6c5c5ded8feb
rohit0147 <rohitchoudhary0147@gmail.com>
2018-09-01 19:07:29 +0530

    q2_v2

--- DIP_lab/160050030_160050031_160050033_assignment3_Segmentation/2/code/myMeanShiftSegmentation.m
+++ DIP_lab/160050030_160050031_160050033_assignment3_Segmentation/2/code/myMeanShiftSegmentation.m
@@ -1,43 +1,36 @@
-function [] = myMeanShiftSegmentation(filename,hr,hs,k)
+% function [] = myMeanShiftSegmentation(filename,hr,hs,k)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 %%%%%%%%Test block%%%%%%%%%%%%%
-% filename = '../data/baboonColor.png';
-% hr = 10;
-% hs = 10;
+filename = '../data/baboonColor.png';
+ hr = 80;
+ hs = 40;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % read image
 im= imread(filename);
-
+% im=mat2gray(im);
 % filter image
 im = imgaussfilt(im,1);
-
-% A = fspecial('gaussian',[5,5], 1);
-% convolution = imfilter(im,A,'conv');
-
 % resize image
-im=imresize(im,.25);
+im=imresize(im,.5);
 
 
 [row,col,~] = size(im);
 upImg = zeros(row,col,3);
 % selecting n nearest neighbours
-n = 100;
+n = 75;
 
 % gradient steps
 % tau = 3;
 
-%%
-
 wait = waitbar(0,'Wait');
 
-for z = 1:k
 %%%
     feature=zeros(5,row*col);
 
     for i = 1:row
         for j=1:col
-            feature(:, i*col - col +j)=[i;j;im(i,j,1); im(i,j, 2); im(i,j,3)];
+            feature(:,i*col - col +j)=[i;j;im(i,j,1); im(i,j, 2); im(i,j,3);];
         end
     end
     X=feature';
@@ -47,36 +40,48 @@ for z = 1:k
     X = cast(X,'double');
     X(:, 3:5) = X(:, 3:5)/(hr);
     X(:, 1:2) = X(:, 1:2)/(hs);
-   	Mdl = KDTreeSearcher(X);
-    
-    [idx,D] = knnsearch(Mdl,cast(X,'double'),'K',n); % finding indexes of k nearest nbrs to (i,j) pixel by color
-    
+   	
+for z = 1:5
+    Mdl = KDTreeSearcher(X);
+    [idx,D] = knnsearch(Mdl,X,'K',n); % finding indexes of k nearest nbrs to (i,j) pixel by color
+    weight = exp(-(D.^2));
+    % upImg=zeros(row,col,3);
     for i = 1:row
         for j = 1:col
             p=i*col-col+j;
-            
-            res=exp(-D(p,:).^2);
+            res=weight(p,:);
+            suma=sum(res);
+%%%%%%%%%%%%%%%%%%%%%%%yeh bhiu dekh%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
             % res=res';
+            % res_mat=[res,res,res];
+            % X(p,3:5)=sum(res_mat.*X(idx(p,:),3:5))/suma;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%            
+            
 %             fun = @(x,y) appKernal(x,i,j,y,hs,hr);
 %             res = arrayfun(fun,idx,D); % each element in res is K(space)*K(color) for each nearest nbr to (i,j)
 %              fun2 = @(x,y) X(x*col-col+j,3:5)*[y,y,y];
 %              smth = arrayfun(fun2,idx,res,'un',0);
 %         meanShift = sum(smth)/sum(res) - im(i,j,:);
-             fun = @(x,y) X(x,3:5)*y*hr;
-             temp= arrayfun(fun,idx(p,:),res,'un',0);
-
-            rgb = sum(reshape(cell2mat(temp),3,[]))/sum(res);
-            upImg(i,j,1) = rgb(1);
-            upImg(i,j,2) = rgb(2);
-            upImg(i,j,3) = rgb(3);
+            fun = @(x,y) X(x,3:5)*y;
+            temp= arrayfun(fun,idx(p,:),res,'un',0);
+            X(p,3:5) = sum(reshape(cell2mat(temp),3,[]),2)/suma;
+%             upImg(i,j,1) = rgb(1);
+%             upImg(i,j,2) = rgb(2);
+%             upImg(i,j,3) = rgb(3);
         end
     end
-    im = upImg;
+    % im = upImg;
     waitbar(z/20,wait,'chalne do');
 end
 close(wait)
+final_im = zeros(row,col,3);
+for i = 1:row
+    for j = 1:col
+        final_im(i,j,:) = X(i*col-col+j,3:5);
+    end
+end
 %%
 figure(1);
-imshow(im);
-end
+imshow(final_im);
+% end
 
