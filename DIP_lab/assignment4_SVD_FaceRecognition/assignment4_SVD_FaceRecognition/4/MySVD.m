86649bda8883a36c5b1b0acf06f2017d859d717e
rajatmajoka <rajat.majoka@gmail.com>
2018-10-14 17:17:49 +0530

    changes in q4

--- DIP_lab/assignment4_SVD_FaceRecognition/assignment4_SVD_FaceRecognition/4/MySVD.m
+++ DIP_lab/assignment4_SVD_FaceRecognition/assignment4_SVD_FaceRecognition/4/MySVD.m
@@ -1,18 +1,24 @@
 % A = [1 2 3;3 5 7;7 5 9;8 9 5];
-function[U ,S,V] =MySVD(A)
+ function[U ,S,V] =MySVD(A)
 [ V1,S1] = eig(A'*A);
 
 [m n] = size(A);
+    S = zeros(m,n);
+    [m1, n1] = size(S1);
 
-S = S1.^(.5);
-S=fliplr(fliplr(S)');
+for i = 1:min(m,n)
+        S(i,i) = sqrt(S1(m1-i+1,m1-i+1));
+end
 V = fliplr(V1);
 
-U = zeros(m,n);    
-for i = 1:1:n
+U = zeros(m,m);    
+for i = 1:min(n,m)
+        if S(i,i)~=0
         U(:,i) = (A*V(:,i))/S(i,i); 
-        
+        else
+            U(:,i) = (A*V(:,i));
+        end    
 end
 
-end
-% Result=U*S*V';
+ end
+%  Result=U*S*V';
